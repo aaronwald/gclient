9acd0709e4fe9cf4bc836525e9cfb2aacb727886
Aaron Wald <aaronwald@gmail.com>
2019-02-12 23:42:51 +0000

    test vs coypu server

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ go get -u google.golang.org/grpc
 cd $GOHOME/src/
 protoc --go_out=plugins=grpc:. -I /home/wald/dev/coypu/src/proto/ coincache.proto
 ```
+
+## Debug
+```
+export GODEBUG=http2debug=2
+```
--- main.go
+++ main.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"nutriasoft.com/coypu/msg"
 	"google.golang.org/grpc"
+	"github.com/golang/protobuf/proto"
 	"context"
 	"time"
 )
 
 
 const (
-	address     = "localhost:8080"
-	defaultName = "world"
+	address     = "localhost:8089"
+	defaultPair = "BTC/USD"
 )
 
 
@@ -23,7 +24,7 @@ func main() {
 		Type: msg.CoypuRequest_BOOK_SNAPSHOT_REQUEST,
 		Message: &msg.CoypuRequest_Snap {
 			Snap: &msg.BookSnapshot {
-				Key: "BTC/USD",
+				Key: defaultPair,
 				Source: 1,
 			},
 		},
@@ -37,11 +38,12 @@ func main() {
 	defer conn.Close()
 	c := msg.NewCoypuServiceClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
 	r, err := c.RequestData(ctx, test)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Coypu Request Type: %d", r.Type)
+	fmt.Println(proto.MarshalTextString(r))
 }
